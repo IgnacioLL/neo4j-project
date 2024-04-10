35167eff63efe20decdbe76e54f3cdbd2e129d38
Ignacio Lloret <ig.lloret.l@gmail.com>
2024-04-10 18:58:47 +0200

    refactor(script): change to run in py script

--- TASK_A2_load_data/script.py
+++ TASK_A2_load_data/script.py
@@ -1,9 +1,26 @@
 from neo4j import GraphDatabase
+from queries import (
+    load_authors,
+    load_article,
+    create_relation_authors_article,
+    load_time,
+    load_journals,
+    load_cities,
+    load_conference_name,
+    load_conference_edition,
+    create_conference_edition_relation,
+    create_city_edition_relation,
+    create_relation_paper_journal,
+    create_relation_year_journal,
+    create_citations_data,
+    load_communities,
+    create_relation_paper_community
+)
 
 # Define the connection URI and credentials
 uri = "bolt://localhost:7687/"  # Your Neo4j URI
 username = "neo4j"  # Your Neo4j username
-password = "12345678"  # Your Neo4j password
+password = "sdm-project"  # Your Neo4j password
 
 # Define a function to run a Cypher query
 def run_query(query):
@@ -18,112 +35,19 @@ MATCH (n)
 DETACH DELETE n
 
 '''
-
-
-query_list = '''
-LOAD CSV WITH HEADERS FROM 'file:///authors_data.csv' AS row
-CREATE (a:Author { 
-    name: row.name,
-    cleaned_name: row.cleaned_name,
-    email: row.email
-});
-
-
-LOAD CSV WITH HEADERS FROM 'file:///article_data.csv' AS row
-CREATE (p:Paper { 
-    year: row.year,
-    id: row.id,
-    title: COALESCE(row.title, ''),
-    url: row.url,
-    volume: row.volume,
-    author_name: row.author_name
-});
-
-
-LOAD CSV WITH HEADERS FROM 'file:///article_authors_relation.csv' AS row
-MATCH (a:Author {id: row.author_id})
-MATCH (p:Paper {id: row.paper_id})
-CREATE (a)-[:AUTHORED]->(p);
-
-// import time
-LOAD CSV WITH HEADERS FROM 'file:///time.csv' AS row
-MERGE (y:Year { value: row.year });
-
-// Import Journals
-LOAD CSV WITH HEADERS FROM 'file:///article_data.csv' AS row
-WITH DISTINCT row.journal AS journal
-MERGE (j:Journal { name: journal });
-    
-
-// Import Cities
-LOAD CSV WITH HEADERS FROM 'file:///city_data.csv' AS row
-MERGE (ct:City { name: row.city });
-
-// Import Conferences
-LOAD CSV WITH HEADERS FROM 'file:///conference_data.csv' AS row
-WITH row WHERE row.conference_name IS NOT NULL
-MERGE (c:Conference { conference_name: row.conference_name })
-SET c.name = COALESCE(row.conference_name, '');
-
-// Import Conference Editions
-LOAD CSV WITH HEADERS FROM 'file:///edition_data.csv' AS row
-MERGE (e:Edition { name: row.conference_edition })
-SET e.city = row.city;
-
-// Create Relationships between Conf and Edition
-LOAD CSV WITH HEADERS FROM 'file:///edition_data.csv' AS row
-MATCH (c:Conference { name: row.conference_name })
-MATCH (e:Edition { name: row.conference_edition, city: row.city })
-CREATE (c)-[:HAS_EDITION]->(e);
-
-// Create Relationships between Conferences and Cities
-LOAD CSV WITH HEADERS FROM 'file:///edition_data.csv' AS row
-MATCH (c:Conference { name: row.conference_name })
-MATCH (ct:City { name: row.city })
-MERGE (c)-[:HELD_IN]->(ct);
-
-// Create relationship between Paper and Journal
-LOAD CSV WITH HEADERS FROM 'file:///article_data.csv' AS row
-MATCH (p:Paper { id: row.id })
-MATCH (j:Journal)
-WHERE j.name = row.journal
-MERGE (p)-[:PUBLISHED_IN]->(j);
-
-// Create relationship between year and journal
-LOAD CSV WITH HEADERS FROM 'file:///your_file.csv' AS row
-MATCH (j:Journal { name: row.journal })
-MATCH (y:Year { value: row.year })
-MERGE (j)-[:OCCURRED_DURING]->(y);
-
-
-
-
-LOAD CSV WITH HEADERS FROM 'file:///citations_data.csv' AS row
-WITH row
-MATCH (p1:Paper { id: row.id })
-MATCH (p2:Paper { id: row.id_cited })
-MERGE (p1)-[:CITES]->(p2);
-
-
-// Create Community nodes
-LOAD CSV WITH HEADERS FROM 'file:///communities_data.csv' AS row
-MERGE (c:Community { category: row.category });
-
-// Create relationship between Paper and Community
-LOAD CSV WITH HEADERS FROM 'file:///communities_data.csv' AS row
-MATCH (p:Paper { id: row.id })
-MATCH (c:Community { category: row.category })
-MERGE (p)-[:HAS_TOPIC]->(c);
-
-
-
-
-'''
-
-
-
-
-# Execute the query
-run_query(cypher_query_data)
-
-result = run_query(cypher_query)
+run_query(clean_slate)
+run_query(load_authors)
+run_query(load_article)
+run_query(create_relation_authors_article)
+run_query(load_time)
+run_query(load_journals)
+run_query(load_cities)
+run_query(load_conference_name)
+run_query(load_conference_edition)
+run_query(create_conference_edition_relation)
+run_query(create_city_edition_relation)
+run_query(create_relation_paper_journal)
+run_query(create_relation_year_journal)
+run_query(create_citations_data)
+run_query(load_communities)
+run_query(create_relation_paper_community)
